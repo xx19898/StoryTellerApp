1bfc15aee987fa2ff4db9c4dd6280536909b1eba
xx19898 <zomg13912111@gmail.com>
2023-07-02 17:27:42 +0300

    fixed errors which occured due to little incompatibility between gsap and reacthookform, made it work by sharing the ref on the inputs between both libraries

--- FrontEnd/src/pages/signUp/signUpPage.tsx
+++ FrontEnd/src/pages/signUp/signUpPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react"
 import { Transition } from "react-transition-group"
 import { gsap } from "../../gsap"
-import { useForm } from "react-hook-form"
+import { useForm, } from "react-hook-form"
 
 interface ISignUpForm{
     username: string,
@@ -15,28 +15,53 @@ const SignUpPage = () => {
     },[])
 
     const [pageIsActive,setPageIsActive] = useState(false)
+    const [temp,setTemp] = useState(false)
 
-    const {register,handleSubmit,watch,formState:{errors},} = useForm<ISignUpForm>()
+    const {register,handleSubmit,formState:{errors,isValid}} = useForm<ISignUpForm>({reValidateMode:'onChange',mode:'onChange'})
 
     const headerRef = useRef(null)
     const usernameLabel = useRef(null)
-    const usernameInput = useRef(null)
+    const usernameInput = useRef<HTMLElement | null>(null)
     const passwordLabel = useRef(null)
-    const passwordInput = useRef(null)
+    const passwordInput = useRef<HTMLElement | null>(null)
     const button = useRef(null)
     const mainRef = useRef(null)
     const formComp = useRef(null)
-    console.log({errors})
+
+    const {
+        ref:refForUsernameInput,
+        ...usernameInputRest
+    } = register("username",{minLength:{message:'Length of the username should be at least 4 characters',value:4}})
+
+    const {
+        ref:refForPasswordInput,
+        ...passwordInputRest
+    } = register("password",{minLength:8})
+
+    console.log({isValid:isValid})
+    console.log({errors:errors})
+    
     return(
         <div ref={mainRef} className="w-auto min-h-screen h-auto bg-base font-belanosima flex flex-col justify-center items-center text-white">
             <Transition in={pageIsActive} nodeRef={formComp} onEnter={onEnter} timeout={400}>
             <form ref={formComp} onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-3 grid-rows-3 gap-2 justify-center items-center py-[4rem] w-[90%] rounded-md pl-4 pr-8 bg-secondary">
             <h2 ref={headerRef} className="text-xl col-span-3 mx-auto my-4">Enter your information</h2>
             <label ref={usernameLabel} className="mx-auto col-span-1 col-start-0">Username</label>
-            <input {...register("username",{minLength:4})} ref={usernameInput} className="input col-span-2 col-start-0 bg-white text-black"/>
+            
+            <input {...usernameInputRest} defaultValue={''} ref={(e) => {
+                refForUsernameInput(e)
+                usernameInput.current = e
+            }} className="input col-span-2 col-start-0 bg-white text-black"/>
             <label ref={passwordLabel} className="mx-auto col-span-1 col-start-0">Password</label>
-            <input {...register("password",{minLength:8})} ref={passwordInput} type={'password'} className="input col-span-2 bg-white text-black"/>
+            
+            <input {...passwordInputRest} ref={(e) => {
+                refForPasswordInput(e)
+                passwordInput.current = e
+            }}
+                type={'password'} className="input col-span-2 bg-white text-black"/>
+            
             <button ref={button} className="text-black hover:bg-primary col-span-3 mx-auto mt-4 btn-secondary bg-white py-4 px-8 rounded-md">Sign Up</button>
+            
             </form>
             </Transition>
         </div>
