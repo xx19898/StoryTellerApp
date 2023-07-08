bde4683284198113d6c3327da2d74658665d3224
xx19898 <zomg13912111@gmail.com>
2023-07-08 21:03:28 +0300

    implemented some validation on login form

--- FrontEnd/src/pages/login/loginPage.tsx
+++ FrontEnd/src/pages/login/loginPage.tsx
@@ -1,9 +1,18 @@
 import { useEffect, useRef, useState } from "react"
 import { gsap } from "gsap/src" 
 import { Transition } from "react-transition-group"
+import { useForm } from "react-hook-form"
+import ErrorComponent from "../../common/forms/ErrorComponent"
+
+interface ILoginForm{
+    username:string,
+    password:string,
+}
 
 
 const LoginPage = ({login}:{login:(username:string,password:string) => void}) => {
+    const {register,clearErrors,formState:{errors,isValid},getValues,handleSubmit} = useForm<ILoginForm>({mode:"onChange",reValidateMode:"onChange"})
+    console.log({errors})
     
     useEffect(() => {
        setPageActive(true)
@@ -14,19 +23,37 @@ const LoginPage = ({login}:{login:(username:string,password:string) => void}) =>
     const headerRef = useRef(null)
     const form = useRef(null)
     const usernameLabel = useRef(null)
-    const usernameInput = useRef(null)
+    const usernameInput = useRef<HTMLElement | null>(null)
     const passwordLabel = useRef(null)
-    const passwordInput = useRef(null)
+    const passwordInput = useRef<HTMLElement | null>(null)
     const button = useRef(null)
+
+    const {ref:usernameInputRefForValid,...usernameInputRest} = register('username',{minLength:{value:4,message:'Username too short'}})
+
+    const {ref:passwordInputRefForValid,...passwordInputRest} = register('password',{minLength:{value:8,message:'Password too short'}})
 
     return (
         <div className="min-h-screen h-auto w-full bg-base text-white font-belanosima flex flex-col justify-center items-center">
             <Transition timeout={400} in={pageIsActive} mountOnEnter unmountOnExit nodeRef={form} onEnter={onEnter}>
             <form ref={form} className="bg-secondary w-[90%] py-[4rem] px-4 grid grid-cols-3 gap-2 grid-rows-3 rounded-md">
                 <h2 ref={headerRef} className="col-start-0 col-span-3 text-xl mx-auto">Welcome, please <u className="text-primary decoration-transparent">login</u></h2>
-                <label ref={usernameLabel} className="col-start-0 col-span-1 flex flex-col justify-center items-center">Username</label><input ref={usernameInput} className="col-start-0 input col-span-2"></input>
-                <label ref={passwordLabel} className="col-start-0 col-span-1 flex flex-col justify-center items-center">Password</label><input ref={passwordInput} type='password' className="col-start-0 input col-span-2"></input>
-                <button ref={button} className="col-start-2 col-span-1s mt-4 bg-white text-black rounded-md py-4">Sign In</button>
+                <label ref={usernameLabel} className="col-start-0 col-span-1 flex flex-col justify-center items-center">Username</label>
+                <input {...usernameInputRest} ref={(e) => {
+                    usernameInputRefForValid(e)
+                    usernameInput.current = e
+                    }} className="col-start-0 input col-span-2"></input>
+                {
+                errors.username ? <div className="col-span-2 col-start-2"><ErrorComponent errorMessage='Username is too short' /></div> : null
+                }
+                <label ref={passwordLabel} className="col-start-0 col-span-1 flex flex-col justify-center items-center">Password</label>
+                <input {...passwordInputRest} ref={(e) => {
+                    passwordInputRefForValid(e)
+                    passwordInput.current = e
+                    }}  type='password' className="col-start-0 input col-span-2"></input>
+                {
+                errors.username && errors.username.type === 'password' ? <div className="col-span-2 col-start-2"><ErrorComponent errorMessage='Password is too short' /></div> : null
+                }
+                <button ref={button} disabled={!isValid} className="col-start-2 col-span-1s mt-4 bg-white text-black rounded-md py-4">Sign In</button>
             </form>
             </Transition>
         </div>
