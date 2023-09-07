8d5e33372009588e1aedb463121cb0165c7e9cc6
xx19898 <zomg13912111@gmail.com>
2023-09-07 18:07:22 +0300

    started dockerizing backend portion of the app(development)

--- FrontEnd/dev.Dockerfile
+++ FrontEnd/dev.Dockerfile
@@ -2,6 +2,10 @@ FROM node:16.15.1 as INITIAL
 WORKDIR /frontEnd
 COPY package.json /frontEnd/
 RUN npm install
-COPY . /frontEnd/
+
+RUN mkdir /frontEnd/node_modules/.vite \
+    && chown -R node:node /frontEnd/node_modules/.vite
+
 EXPOSE 3000
+
 CMD ["npm","run","dev"]
--- FrontEnd/src/pages/introPage.tsx
+++ FrontEnd/src/pages/introPage.tsx
@@ -38,7 +38,7 @@ const Welcome = () => {
                 <div  className="w-full min-h-screen bg-base flex flex-col justify-center items-center font-belanosima">
                     <div ref={mainRef} className=" p-4 h-auto bg-secondary flex flex-col justify-center items-center rounded-md mx-10 py-[10%] shadow-3xl">
                         <h1 ref={headerRef} className="text-4xl font-normal text-white my-4">Hello, <u className="text-primary decoration-transparent">stranger</u></h1>
-                        <p className="indent-4 text-white text-xl w-auto mb-4">Do you have a <u className="text-primary decoration-transparent">story</u> to <u className="text-primary decoration-transparent">tell?</u> Or maybe you would be interested to read the stories of others then?
+                        <p className="indent-4 text-white text-xl w-auto mb-4">Do you have a <u className="text-primary decoration-transparent">story</u> to <u className="text-primary decoration-transparent">tell?</u> Or maybe you would be interested to read the stories of others?
                         Either way, we've got you <u className="text-primary decoration-transparent">covered</u>. Sign in or sign up and get going! </p>
                         <button ref={actionButtonRef} onClick={() => setAction(true)}
                         className="btn-neutral rounded-md px-8 py-4 text-lg mb-4">
