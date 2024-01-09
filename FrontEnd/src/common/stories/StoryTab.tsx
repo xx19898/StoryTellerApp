16736c46f3f657a788c790c723b95052a2d2acca
xx19898 <zomg13912111@gmail.com>
2024-01-09 11:47:37 +0200

    done work on storytabs

--- FrontEnd/src/common/stories/StoryTab.tsx
+++ FrontEnd/src/common/stories/StoryTab.tsx
@@ -1,3 +1,7 @@
+import { LiaComments } from "react-icons/lia";
+import { FaHeart } from "react-icons/fa";
+import { v4 as uuidv4 } from 'uuid';
+
 
 export interface IStoryTab{
     ID:number,
@@ -9,13 +13,24 @@ export interface IStoryTab{
 
 const StoryTab = ({Owner,Title,NumberComments,NumberOfLikes}:IStoryTab) => {
     return (
-        <li key={Title} className="bg-special w-full h-full flex flex-row gap-5 text-white justify-center align-center p-4 rounded-lg my-2">
-            <p>{Owner}</p>
-            <p>{Title}</p>
-            <p>{NumberOfLikes}</p>
+        <li key={uuidv4()} onClick={(e) => console.log('clicked')} className="bg-special hover:cursor-pointer w-full h-full flex flex-col gap-5 text-white justify-center align-center p-4 rounded-lg my-2">
+            <p className='font-semibold text-xl text-center'>{Title}</p>
+            <div className="w-full h-full flex flex-row gap-6 text-white justify-center align-center">
+                <div className="flex flex-row justify-center align-center gap-2">
+                    <FaHeart size={30}/>
+                    <p>{NumberOfLikes}</p>
+                </div>
+                <div className="flex flex-row justify-center align-center gap-2">
+                    <LiaComments size={30}/>
+                    <p>{NumberComments}</p>
+                </div>
+                <div className="flex flex-row text-center align-center justify-center gap-2 ml-4">
+                    <p >Author:</p>
+                    <p className="font-semibold text-md relative text-center">{Owner}</p>
+                </div>
+            </div>
         </li>
     )
-
 }
 
 export default StoryTab
