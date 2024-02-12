620f3140596190aa5c94d5a1271dfda635c6809e
xx19898 <xx1989@github.com>
2024-02-12 22:23:36 +0200

    added loading spinner to editing input and animated it

--- FrontEnd/src/modules/textEditor/EditingInput.tsx
+++ FrontEnd/src/modules/textEditor/EditingInput.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react"
 import { SlActionUndo } from "react-icons/sl";
 import { SlCheck } from "react-icons/sl";
+import LoadingSpinner from "./LoadingSpinner";
 
 interface IEditingInput{
     origValue: string,
@@ -39,22 +40,21 @@ const EditingInput = ({identifier,edit,origValue,stopEditing}:IEditingInput) => 
     }
 
     return(
-        <div className="w-full h-auto p-4 flex flex-col justify-center items-center bg-darkerSecondary rounded-md">
+        <div className="w-full h-auto flex flex-col justify-center items-center rounded-md outline outline-1 outline-white">
             {
-                storyUpdating ? <p>UPDATING</p>: null
+                storyUpdating ? 
+                <LoadingSpinner />
+                :
+                null
             }
             <textarea 
-            className="indent-4 p-2 h-auto w-full text-black focus:outline-none rounded-md" 
+            className="indent-4 p-2 h-auto w-full text-white bg-secondary focus:outline-none rounded-md resize-none" 
             defaultValue={inputValue}
             style={{height: textAreaHeight === 'auto' ? 'auto' : `${textAreaHeight}px`}} 
             onChange={(e) => onChange(e.target.value)}
             ref={textAreaRef}
             >
             </textarea>
-            <section className="flex flex-row gap-5 pt-3 justify-center items-center">
-                <SlActionUndo size='1.5rem'/>
-                <SlCheck size='1.5rem'/>
-            </section>
         </div>
     )
 }
---
+++ FrontEnd/src/modules/textEditor/LoadingSpinner.tsx
@@ -0,0 +1,18 @@
+import { IoIosSync } from "react-icons/io";
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+
+
+export default () => {
+    const spinnerRef = useRef<HTMLDivElement>(null)
+    useGSAP(() => {
+        gsap.to(spinnerRef.current,{rotation:180,repeat:-1,duration:3})
+    })
+
+    return (
+    <div ref={spinnerRef}>
+        <IoIosSync ref={spinnerRef} size={50}/>
+    </div>
+    )
+}
