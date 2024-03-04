0986c666369613e8b83102aabcaf5c19296fb993
xx19898 <xx1989@github.com>
2024-03-04 21:58:01 +0200

    image stories folders creation progress

--- BackEnd/image_storage/image_file_helper.go
+++ BackEnd/image_storage/image_file_helper.go
@@ -7,6 +7,10 @@ import (
 	"strconv"
 )
 
+func goBackToImageStorageDir() {
+	os.Chdir("/backend/image_storage")
+}
+
 func CheckThatDirectoryExists(path string) bool {
 	if stat, err := os.Stat(path); err == nil && stat.IsDir() {
 		return true
@@ -25,9 +29,13 @@ func getParentPath() string {
 func StoriesImagesFolderForUserExists(username string) bool {
 	parent := getParentPath()
 	userFolderPath := filepath.Join(parent, "IMAGES", "stories", username)
-	fmt.Println("*****")
-	fmt.Println(userFolderPath)
-	fmt.Println("*****")
+	return CheckThatDirectoryExists(userFolderPath)
+}
+
+func UserFolderForImagesExists(username string) bool {
+	parent := getParentPath()
+	userFolderPath := filepath.Join(parent, "IMAGES", "stories", username)
+
 	return CheckThatDirectoryExists(userFolderPath)
 }
 
@@ -47,11 +55,177 @@ func CreateUserDirInStoriesFolder(username string) bool {
 	return true
 }
 
-func DeleteUserDirInStoriesFolder(username string) {
-	os.Chdir("..")
-	os.Chdir("IMAGES")
-	os.Chdir("stories")
-	os.Remove(username)
+/*
+func createStoryDir(username string, storyId uint) bool {
+
+}
+
+func CreateFolderForStoryIds(username string, storyId uint) {
+	userFolderExists := UserFolderForImagesExists(username)
+
+	if !userFolderExists {
+		CreateUserDirInStoriesFolder(username)
+	}
+
+	createStoryDir(username, storyId)
+}
+
+*/
+
+func DeleteUserDirInStoriesFolder(username string) error {
+	var err error
+
+	err = os.Chdir("../")
+
+	if err != nil {
+		return err
+	}
+
+	err = os.Chdir("IMAGES")
+
+	if err != nil {
+		return err
+	}
+
+	err = os.Chdir("stories")
+
+	if err != nil {
+		return err
+	}
+
+	err = os.Remove(username)
+
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func DeleteStoryFolder(username string, storyId uint) error {
+	var err error
+	err = os.Chdir("../")
+
+	if err != nil {
+		goBackToImageStorageDir()
+		return err
+	}
+
+	err = os.Chdir("IMAGES")
+
+	if err != nil {
+		goBackToImageStorageDir()
+		return err
+	}
+
+	err = os.Chdir("stories")
+
+	if err != nil {
+		goBackToImageStorageDir()
+		return err
+	}
+
+	err = os.Chdir(username)
+
+	if err != nil {
+		goBackToImageStorageDir()
+		return err
+	}
+
+	err = os.Remove(fmt.Sprint(storyId))
+
+	goBackToImageStorageDir()
+
+	return err
+}
+
+func CreateStoryFolder(username string, storyId uint) error {
+
+	var err error
+
+	if StoriesImagesFolderForUserExists(username) {
+		err = os.Chdir("../")
+
+		if err != nil {
+			return err
+		}
+
+		x, _ := os.Getwd()
+
+		fmt.Println("*******")
+		fmt.Println(x)
+		fmt.Println("*******")
+
+		err = os.Chdir("IMAGES")
+
+		if err != nil {
+			os.Chdir("./image")
+			return err
+		}
+
+		err = os.Chdir("stories")
+
+		if err != nil {
+			return err
+		}
+
+		err = os.Chdir(username)
+
+		if err != nil {
+			return err
+		}
+
+		err = os.Mkdir(fmt.Sprint(storyId), 0755)
+
+		if err != nil {
+			return err
+		}
+
+		return err
+	} else {
+		CreateUserDirInStoriesFolder(username)
+
+		currDir, _ := os.Getwd()
+		os.Chdir(currDir)
+
+		currDir, _ = os.Getwd()
+
+		fmt.Println("****")
+		fmt.Println(currDir)
+		fmt.Println("****")
+
+		err = os.Chdir("../")
+
+		if err != nil {
+			return err
+		}
+
+		err = os.Chdir("IMAGES")
+
+		if err != nil {
+			return err
+		}
+
+		err = os.Chdir("stories")
+
+		if err != nil {
+			return err
+		}
+
+		err = os.Chdir(username)
+
+		if err != nil {
+			return err
+		}
+
+		err = os.Mkdir(fmt.Sprint(storyId), 0755)
+
+		if err != nil {
+			return err
+		}
+
+		return err
+	}
 }
 
 //func for creating func for creating folder with storyid name exists
--- BackEnd/image_storage/image_file_helper_test.go
+++ BackEnd/image_storage/image_file_helper_test.go
@@ -65,3 +65,74 @@ func TestThatFolderCreationForUserWorks(t *testing.T) {
 		t.Fatal("folder tu should not exist inside IMAGES/stories")
 	}
 }
+
+func TestThatStoryFolderExistanceCheckWorks(t *testing.T) {
+	res := StoriesImagesFolderForStoryIdExists("testUser", 1)
+
+	if !res {
+		t.Fatal("Folder stories/testUser/1 should exist")
+	}
+
+	res = StoriesImagesFolderForStoryIdExists("testUser", 2)
+
+	if res {
+		t.Fatal("Folder stories/testUser/2 should not exist")
+	}
+}
+
+// TODO: test creation of story folder inside IMAGES/username directory
+func TestThatStoryFolderGetsCreatedAndDeletedCorrectly(t *testing.T) {
+	res := StoriesImagesFolderForStoryIdExists("ts", 1)
+
+	mydir, _ := os.Getwd()
+
+	os.Chdir(mydir)
+
+	if res {
+		t.Fatal("Folder stories/ts/1 should not exist")
+	}
+
+	err := CreateStoryFolder("ts", 1)
+
+	if err != nil {
+		fmt.Println(err)
+		t.Fatal("Could not create folder ts/1 in stories")
+	}
+
+	os.Chdir(mydir)
+
+	err = DeleteStoryFolder("ts", 1)
+
+	if err != nil {
+		fmt.Println(err)
+		t.Fatal("Could not delete folder ts/1 in stories")
+	}
+
+	os.Chdir(mydir)
+
+	res = StoriesImagesFolderForStoryIdExists("ts", 1)
+
+	if !res {
+		t.Fatal("stories/ts/1 still exists after the deletion")
+	}
+}
+
+/*
+
+func TestThatStoryFolderGetsCreatedCorrectly(t *testing.T) {
+
+}
+
+// TODO: create functionality and test for creating new image under non existing user and image name
+func TestThatImageCreatingForNonExistantUsersWorks(t *testing.T) {
+	testImageBytes, err := os.ReadFile("../IMAGES/test_image_sun.jpg")
+
+	if err != nil {
+		t.Fatal("unable to read file")
+	}
+
+	//Create directory for said user
+
+}
+
+*/
