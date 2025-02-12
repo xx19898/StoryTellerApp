20af583c2ced1247c2994513a305aca433194c02
xx19898 <xx1989@github.com>
2025-02-12 22:08:13 +0200

    some more code in story parsing

--- BackEnd/stories/story_helpers.go
+++ BackEnd/stories/story_helpers.go
@@ -1,7 +1,32 @@
 package stories
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
+func getAllowedElementsAndPropertiesMap()(map[string][]string,error){
+	allowedElementTagsWithProperties := map[string] []string{
+		"h":[]string{},
+		"p":[]string{},
+		"img":[]string{"src"},}
+	return allowedElementTagsWithProperties,nil
+}
+type elementsPropertiesFunctionType func() (map[string][]string,error)
+/*
+
+make(
+	map[string][]string{
+		"h":[]string{},
+		"p":[]string{},
+		"img":[]string{"src"},
+}
+)
+*/
+
+
+
+//checks that story is not empty nor too long
 func prelimCheckStory(story string) (bool, error) {
 	if len(story) > 500 {
 		return false, errors.New("Story too long")
@@ -11,19 +36,33 @@ func prelimCheckStory(story string) (bool, error) {
 	}
 	return true, nil
 }
-func getTypeOfElement(element string) (string, error){
+
+func getTypeOfElement(element string,getElementsPropertiesMap elementsPropertiesFunctionType) (string, error){
+	trimmedElement := strings.TrimSpace(element)
+	  
+	if(len(trimmedElement) == 0){
+		return "",  errors.New("Element is empty")
+	}
 	
+	allowedElementsAndPropertiesMap,err := getElementsPropertiesMap()
+	if(err != nil){
+		return "",errors.New("Could not get allowedElementsAndPropertiesMap")
+	}
+	//HERE
 }
+
+/*
 func SanitizeStoryHtmlString(unsntzd string) (string,error) {
 	storyIsOk,err := prelimCheckStory(unsntzd)
 	if(!storyIsOk || err != nil){
 		return "",err
 	} 
 	
-	let currElementType = ""
+	var currElementType = ""
+	
 	//element type can only be h1,2,3..., p, img
 	for index, char := range unsntzd {
 
 	}
 }
-
+*/
