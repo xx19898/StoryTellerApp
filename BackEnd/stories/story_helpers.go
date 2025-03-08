895a5a3a052f03d3eed39cc78c1092ca889d2b5d
xx19898 <xx1989@github.com>
2025-03-08 18:29:47 +0200

    split type extraction function into smaller functions for separation of concerns

--- BackEnd/stories/story_helpers.go
+++ BackEnd/stories/story_helpers.go
@@ -28,14 +28,65 @@ make(
 
 
 //checks that story is not empty nor too long
-func prelimCheckStory(story string) (bool, error) {
+func prelimCheckStory(story []rune) error {
 	if len(story) > 500 {
-		return false, errors.New("Story too long")
+		return errors.New("Story too long")
 	}
 	if(len(story) == 0) {
-		return false, errors.New("Story is empty")
+		return errors.New("Story is empty")
 	}
-	return true, nil
+	return nil
+}
+
+func CheckStorySynthaxis(story string) (error){
+	trimmedElement := []rune(strings.TrimSpace(story))
+
+
+	
+	if(len(trimmedElement) == 0){
+		return errors.New("Element is empty")
+	}
+	
+	/*
+	allowedElementsAndPropertiesMap,err := getElementsPropertiesMap()
+	if(err != nil){
+		return "",errors.New("Could not get allowedElementsAndPropertiesMap")
+	}
+	*/
+
+	//TODO: check that <> </> are ok, what of escaped <'s and >'s?
+	
+	firstChar := string(trimmedElement[0])
+	if(firstChar != "<") {
+		fmt.Println("first char is  " + firstChar)
+		return errors.New("First char is not \"<\" " + "but " + firstChar)
+	}
+
+	lastChar := string(trimmedElement[len(trimmedElement) - 1])
+	if(lastChar != ">"){
+		return errors.New("Last char " + "(" + lastChar + ")" + "is not \">\"")
+	}
+
+	// loop through characters, not bytes
+	secondLastChar := string(trimmedElement[len(trimmedElement) - 2])
+	if(secondLastChar != "/"){
+		return errors.New("Second last char is not \"\\\"")
+	}
+
+	// check that html element is semantically proper: <> </>
+	var checkString strings.Builder
+	for _,char := range trimmedElement{
+		for _,keyElement := range []rune{'>','<','/'}{
+			if(char == keyElement){
+				checkString.WriteRune(char)
+			}
+		}
+	}
+	if checkString.String() != "<></>"{
+		return errors.New("the html element is not semantically proper: <></>")		
+	}
+	
+	return nil
 }
 
 func GetTypeOfElement(element string,getElementsPropertiesMap elementsPropertiesFunctionType) (string, error){
--- BackEnd/stories/story_parsing_test.go
+++ BackEnd/stories/story_parsing_test.go
@@ -4,16 +4,37 @@ import (
 	"fmt"
 	"testing"
 )
-
-func TestHtmlElementTypeExtraction(t *testing.T) {
+func TestStoryHtmlSynthaxisIsCorrect(t *testing.T){
 	testElement := "<wrong>"
 	_,err := GetTypeOfElement(testElement,GetAllowedElementsAndPropertiesMap)
 	
 	if err == nil {
-		fmt.Printf("Function did not see the error in %s",testElement)
-		t.Fatal(err.Error())
+		t.Fatalf(fmt.Sprintf("Function did not see the error in %s",testElement))
 	}
 	
 	fmt.Print(err.Error())
-	//test that <div><div>,<div></p> does not go through
+	
+	testElement = "<div><div>"
+	_,err = GetTypeOfElement(testElement,GetAllowedElementsAndPropertiesMap)
+	fmt.Println(err.Error())
+	if err == nil{
+		t.Fatalf(fmt.Sprintf("Function did not see the error in %s , missing / before the last >",testElement))
+	}
+
+	testElement = "<<div><div/>"
+	_,err = GetTypeOfElement(testElement,GetAllowedElementsAndPropertiesMap)
+	if err == nil{
+		t.Fatalf(fmt.Sprintf("Function did not see the error in %s ,there is no such html elements as wrong",testElement))
+	}
+}
+func TestHtmlElementTypeExtraction(t *testing.T) {
+	testElement := "<div><div/>"
+	elementType,err := GetTypeOfElement(testElement, GetAllowedElementsAndPropertiesMap)
+	if err == nil{
+		t.Fatalf(fmt.Sprintf("Function did not see the error in %s ,there is no such html elements as wrong",testElement))
+	}
+	if elementType != "div"{
+		t.Fatalf("Incorrect element type is extracted")		
+	}
+
 }
